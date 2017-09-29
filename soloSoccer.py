89368d959afbf73c86950f0f3ea1c029ab4f59ed
Checkmunza <check.nokia5233@gmail.com>
2017-09-29 23:51:34 +0700

    ModelSprite can sync angle

--- soloSoccer.py
+++ soloSoccer.py
@@ -1,7 +1,22 @@
 import arcade
+from models import World, Player
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+            self.angle = self.model.angle
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class SoccerWindow(arcade.Window):
     def __init__(self, width, height):
@@ -10,20 +25,36 @@ class SoccerWindow(arcade.Window):
         self.backgound = None
         arcade.set_background_color(arcade.color.GREEN)
 
+        # Create Sprite
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.player1_sprite = ModelSprite('images/player1.png',model=self.world.player1)
+
     def setupStadium(self):
         # Set Backgound to field
         self.backgound = arcade.load_texture("images/field.png")
 
     def on_draw(self):
         arcade.start_render()
-
         # Draw backgound
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.backgound)
+        # Draw Sprite
+        self.player1_sprite.draw()
+
+    def update(self, delta):
+        # Update Object in World 
+        self.world.update(delta)
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
+
+    def on_key_release(self, key, key_modifiers):
+        self.world.on_key_release(key, key_modifiers)
 
 def main():
     window = SoccerWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
+    # Setup to field
     window.setupStadium()
     arcade.run()
  
