73ab34626e9d215156c10126f127964d911b5df0
rawitP <rawit.p@ku.th>
2017-10-01 02:27:16 +0700

    Ball can move

--- models.py
+++ models.py
@@ -1,11 +1,25 @@
 import math
 import arcade.key
+import random
 
 class Ball:
+    MOVE_ACC = -1
+
     def __init__(self, x, y, angle=0):
         self.x = x
         self.y = y
+        self.speed = 0
         self.angle = angle
+
+    def update(self, delta):
+        # Make it move realistic
+        if self.speed > 0 :
+            self.speed = self.speed + self.MOVE_ACC*(0.1) # v = u + at
+            if self.speed < 0 :
+                self.speed = 0
+            self.x += math.cos(math.radians(self.angle)) * self.speed
+            self.y += math.sin(math.radians(self.angle)) * self.speed
+        
 
 class Player:
     TURN_SPEED = 3
@@ -35,10 +49,11 @@ class World:
         self.width = width
         self.height = height
         self.player1 = Player(width // 2, height // 2, 0)
-        self.ball = Ball(width // 2 * 1.5, height // 2)
+        self.ball = Ball(width // 2 , height // 2)
  
     def update(self, delta):
         self.player1.update(delta)
+        self.ball.update(delta)
 
     def on_key_press(self, key, key_modifiers):
         # Player1 will walk
@@ -49,7 +64,6 @@ class World:
             self.player1.turn_direction[1] = True
         if key == arcade.key.LEFT:
             self.player1.turn_direction[0] = True
-        
 
     def on_key_release(self, key, key_modifiers):
         # Player1 will NOT walk
