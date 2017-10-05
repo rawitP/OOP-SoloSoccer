51c3f29153a6976599d4f91feb6e0360d23ff214
rawitP <rawit.p@ku.th>
2017-10-06 04:45:25 +0700

    Add Goal Sprite and WallSprite Class for ball bouncing.

--- models.py
+++ models.py
@@ -21,12 +21,53 @@ class Ball:
         self.speed = 0
         self.angle = angle
         self.is_outside = False
+        self.prev_x = 0
+        self.prev_y = 0
 
+    def prev_pos(self):
+        self.x = self.prev_x
+        self.y = self.prev_y
+
+    def bounce_reverse(self):
+        self.angle = self.angle + 180
+
+    def bounce_to_top(self):
+        # Go Right
+        if self.angle > 270:
+            self.angle = 90 - (self.angle - 270)
+        # Go Left
+        else:
+            self.angle = 90 + (270 - self.angle)
+
+    def bounce_to_bottom(self):
+        # Go Left
+        if self.angle > 90:
+            self.angle = 270 - (self.angle - 90)
+        # Go Right
+        else:
+            self.angle = 270 + (90 - self.angle)
+
+    def bounce_to_left(self):
+        # Go Down
+        if self.angle > 180:
+            self.angle = 180 + (360 - self.angle)
+        # Go UP
+        else:
+            self.angle = 180 - (self.angle - 0)
+
+    def bounce_to_right(self):
+        # Go Down
+        if self.angle > 180:
+            self.angle = 270 + (270 - self.angle)
+        # Go UP
+        else:
+            self.angle = 0 + (180 - self.angle)
+   
     def update(self, delta):
         '''
         Bouncing at border
         UPDATE: Added goal area for boucing
-        ''' 
+    
         if not self.is_outside:
             # When ball outside line or goal
             if (self.x < self.BORDER_LEFT and
@@ -59,6 +100,7 @@ class Ball:
             elif self.y > self.BORDER_TOP or\
                     (self.y > HEIGHT/2 + self.GOAL_SIZE / 2 and 
                     (self.x < self.BORDER_LEFT or self.x > self.BORDER_RIGHT)):
+                print(self.x,self.y)
                 print("y2")
                 self.is_outside = True
                 if self.angle % 360 > 90:
@@ -70,14 +112,108 @@ class Ball:
             if self.x > self.BORDER_LEFT or self.x < self.BORDER_RIGHT or\
                self.y > self.BORDER_BOTTOM or self.y < self.BORDER_TOP:
                 self.is_outside = False
-        
+        '''
         # Make it move realistic
         if self.speed > 0 :
+            self.prev_x = self.x
+            self.prev_y = self.y
             self.speed = self.speed + self.MOVE_ACC*(0.1) # v = u + at
             if self.speed < 0 :
                 self.speed = 0
+            '''
+            prev_x = self.x
+            prev_y = self.y
+            '''
             self.x += math.cos(math.radians(self.angle)) * self.speed
             self.y += math.sin(math.radians(self.angle)) * self.speed
+            #print('Next Pos',self.x,self.y,self.angle)
+            ###
+            ###
+            '''
+            # new bouncing
+            if self.x > self.BORDER_RIGHT:
+                # OUTSTIDE GOAL 
+                if self.y > HEIGHT/2 + self.GOAL_SIZE/2 or\
+                   self.y < HEIGHT/2 - self.GOAL_SIZE/2 :
+                    self.x = prev_x
+                    self.y = prev_y
+                    print("X > BORDER, OUTSIDE GOAL")
+                    if self.angle % 360 > 180:
+                        self.angle = 270 - (self.angle - 270)
+                    else:
+                        self.angle = 90 + (90 - self.angle)
+                # INSIDE GOAL
+                elif self.x > WIDTH :
+                    self.x = prev_x
+                    self.y = prev_y
+                    print("X > BORDER, INSIDE GOAL")
+                    if self.angle % 360 > 180:
+                        self.angle = 270 - (self.angle - 270)
+                    else:
+                        self.angle = 90 + (90 - self.angle)
+            elif self.x < self.BORDER_LEFT:
+                 # OUTSTIDE GOAL
+                if self.y > HEIGHT/2 + self.GOAL_SIZE/2 or\
+                   self.y < HEIGHT/2 - self.GOAL_SIZE/2 :
+                    self.x = prev_x
+                    self.y = prev_y
+                    print("X < BORDER, OUTSIDE GOAL")
+                    if self.angle % 360 > 180:
+                        self.angle = 270 + (270 - self.angle)
+                    else:
+                        self.angle = 90 - (self.angle - 90)
+                # INSIDE GOAL
+                elif self.x < 0:
+                    self.x = prev_x
+                    self.y = prev_y
+                    print("X < BORDER, INSIDE GOAL")
+                    if self.angle % 360 > 180:
+                        self.angle = 270 + (270 - self.angle)
+                    else:
+                        self.angle = 90 - (self.angle - 90)
+            if self.y >= HEIGHT/2 + self.GOAL_SIZE/2 :
+                # INSIDE GOAL
+                if self.x <= self.BORDER_LEFT or\
+                   self.x >= self.BORDER_RIGHT :
+                    self.x = prev_x
+                    self.y = prev_y
+                    print("Y >= GOAL, INSIDE GOAL")
+                    if self.angle % 360 > 90:
+                        self.angle = 180 + (180 - self.angle)
+                    else:
+                        self.angle = 360 - self.angle
+                # OUTSIDE GOAL
+                elif self.y > self.BORDER_TOP :
+                    self.x = prev_x
+                    self.y = prev_y
+                    print("Y >= GOAL, OUTSIDE GOAL")
+                    if self.angle % 360 > 90:
+                        self.angle = 180 + (180 - self.angle)
+                    else:
+                        self.angle = 360 - self.angle
+            elif self.y <= HEIGHT/2 - self.GOAL_SIZE/2 :
+                # INSIDE GOAL
+                if self.x <= self.BORDER_LEFT or\
+                   self.x >= self.BORDER_RIGHT :
+                    self.x = prev_x
+                    self.y = prev_y
+                    print("Y <= GOAL, INSIDE GOAL")
+                    if self.angle % 360 > 270 :
+                        self.angle = 90 - (self.angle - 270)
+                    else:
+                        self.angle = 90 + (270 - self.angle)
+                # OUTSIDE GOAL
+                elif self.y < self.BORDER_BOTTOM :
+                    self.x = prev_x
+                    self.y = prev_y
+                    print("Y >= GOAL, OUTSIDE GOAL")
+                    if self.angle % 360 > 270 :
+                        self.angle = 90 - (self.angle - 270)
+                    else:
+                        self.angle = 90 + (270 - self.angle)
+            self.angle = self.angle % 360
+            print(self.x,self.y,self.angle)
+            '''        
 
 class Player:
     TURN_SPEED = 3
--- soloSoccer.py
+++ soloSoccer.py
@@ -3,6 +3,9 @@ from models import World, Player
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+BORDER_SIZE = 50
+GOAL_WIDTH = 100
+GOAL_HEIGHT = 200
 TWO_PLAYER = False
 
 class ModelSprite(arcade.Sprite):
@@ -22,6 +25,14 @@ class ModelSprite(arcade.Sprite):
         self.sync_with_model()
         super().draw()
 
+class WallSprite(arcade.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__('images/block.png')
+        self.set_position(x, y)
+        self.width = width
+        self.height = height
+        self.alpha = 0
+
 class SoccerWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -36,6 +47,39 @@ class SoccerWindow(arcade.Window):
                                            model=self.world.player1)
         self.ball_sprite = ModelSprite('images/ball.png', model=self.world.ball)
         
+        # Add Wall for bouncing
+        self.wall_sprite_list = arcade.SpriteList()
+        self.wall_top = WallSprite(SCREEN_WIDTH//2, SCREEN_HEIGHT - BORDER_SIZE//2, 
+                                   SCREEN_WIDTH, BORDER_SIZE)
+        self.wall_bottom = WallSprite(SCREEN_WIDTH//2, 0 + BORDER_SIZE//2, 
+                                      SCREEN_WIDTH, BORDER_SIZE)
+        self.wall_left_top = WallSprite(GOAL_WIDTH//2, SCREEN_HEIGHT//2*2.25,
+                                        GOAL_WIDTH, SCREEN_HEIGHT)
+        self.wall_left_bottom = WallSprite(GOAL_WIDTH//2, SCREEN_HEIGHT//2*-0.25,
+                                           GOAL_WIDTH, SCREEN_HEIGHT)
+        self.wall_left_outside = WallSprite(-BORDER_SIZE//2, SCREEN_HEIGHT//2,
+                                             BORDER_SIZE, SCREEN_HEIGHT)
+        self.wall_right_top = WallSprite(SCREEN_WIDTH - GOAL_WIDTH//2, SCREEN_HEIGHT//2*2.25,
+                                         GOAL_WIDTH, SCREEN_HEIGHT)
+        self.wall_right_bottom = WallSprite(SCREEN_WIDTH - GOAL_WIDTH//2, SCREEN_HEIGHT//2*-0.25,
+                                            GOAL_WIDTH, SCREEN_HEIGHT)
+        self.wall_right_outside = WallSprite(SCREEN_WIDTH + BORDER_SIZE//2, SCREEN_HEIGHT//2,
+                                             BORDER_SIZE, SCREEN_HEIGHT)
+        self.wall_sprite_list.append(self.wall_top)
+        self.wall_sprite_list.append(self.wall_bottom)
+        self.wall_sprite_list.append(self.wall_left_top)
+        self.wall_sprite_list.append(self.wall_left_bottom)
+        self.wall_sprite_list.append(self.wall_left_outside)     
+        self.wall_sprite_list.append(self.wall_right_top)
+        self.wall_sprite_list.append(self.wall_right_bottom)
+        self.wall_sprite_list.append(self.wall_right_outside)   
+
+        # Add Goal Sprites for detect the ball
+        self.goal_area_1 = arcade.Sprite('images/goal.png')
+        self.goal_area_1.set_position(50, SCREEN_HEIGHT//2)
+        self.goal_area_2 = arcade.Sprite('images/goal.png')
+        self.goal_area_2.set_position(SCREEN_WIDTH - (GOAL_WIDTH//2), SCREEN_HEIGHT//2)
+
         ###
         if TWO_PLAYER:
             self.player2_sprite = ModelSprite('images/player1.png',
@@ -51,15 +95,13 @@ class SoccerWindow(arcade.Window):
         # Draw backgound
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.backgound)
-        
-        ### GOAL AREA
-        arcade.draw_lrtb_rectangle_filled(1205,1280,460,260,arcade.color.RED)
-        arcade.draw_lrtb_rectangle_filled(0,75,460,260,arcade.color.RED)
-        ###
-        
+
         # Draw Sprite
+        self.wall_sprite_list.draw()
         self.ball_sprite.draw()
         self.player1_sprite.draw()
+        self.goal_area_1.draw()
+        self.goal_area_2.draw()
 
         ###
         if TWO_PLAYER:
@@ -69,11 +111,109 @@ class SoccerWindow(arcade.Window):
     def update(self, delta):
         # Update Object in World 
         self.world.update(delta)
+        self.ball_sprite.sync_with_model()
         # When Player1 hit the ball
         if arcade.check_for_collision(self.ball_sprite, self.player1_sprite) :
             self.world.ball.speed = self.world.player1.kick_power
             self.world.ball.angle = self.world.player1.angle
         
+        '''
+        Check For collision between ball and walls.
+
+        Concept:
+        + + + + + + +
+        +       # # # # #  |
+        +       #   +   #  | dist_y
+        + + + + # + +   #  |
+                # # # # #
+                _ _ _ 
+                dist_x
+
+        '''
+        hit_walls = arcade.check_for_collision_with_list(self.ball_sprite, 
+                                                         self.wall_sprite_list)
+        for wall in hit_walls:
+            # Right side
+            if self.ball_sprite.right > wall.right:
+                dist_x = wall.right - self.ball_sprite.left
+                if self.ball_sprite.bottom < wall.bottom:
+                    dist_y = self.ball_sprite.top - wall.bottom
+                    if dist_x > dist_y:
+                        #print("Go Bottom_Left")
+                        self.world.ball.bounce_to_bottom()
+                        self.world.ball.prev_pos()
+                    elif dist_x == dist_y:
+                        #print("Go Reverse")
+                        self.world.ball.bounce_reverse()
+                        self.world.ball.prev_pos()
+                    else:
+                        #print("Go Right_Top")
+                        self.world.ball.bounce_to_right()
+                        self.world.ball.bounce_to_right
+                    self.world.ball.prev_pos()
+                elif self.ball_sprite.top > wall.top:
+                    dist_y = wall.top - self.ball_sprite.bottom
+                    if dist_x > dist_y:
+                        #print("Go Top_Left")
+                        self.world.ball.bounce_to_top()
+                        self.world.ball.prev_pos()
+                    elif dist_x == dist_y:
+                        #print("Go Reverse")
+                        self.world.ball.bounce_reverse()
+                        self.world.ball.prev_pos()
+                    else:
+                        #print("Go Right_Bottom")
+                        #print(dist_x, dist_y)
+                        self.world.ball.bounce_to_right()
+                        self.world.ball.prev_pos()
+                else:
+                    #print("Go Right_?")
+                    self.world.ball.bounce_to_right()
+                    self.world.ball.prev_pos()
+            # Left side
+            elif self.ball_sprite.left < wall.left:
+                dist_x = self.ball_sprite.right - wall.left
+                if self.ball_sprite.bottom < wall.bottom:
+                    dist_y = self.ball_sprite.top - wall.bottom
+                    if dist_x > dist_y:
+                        #print("Go Bottom_Right")
+                        self.world.ball.bounce_to_bottom()
+                        self.world.ball.prev_pos()
+                    elif dist_x == dist_y:
+                        #print("Go Reverse")
+                        self.world.ball.bounce_reverse()
+                        self.world.ball.prev_pos()
+                    else:
+                        #print("Go LEFT_Top")
+                        self.world.ball.bounce_to_left()
+                        self.world.ball.prev_pos()
+                elif self.ball_sprite.top > wall.top:
+                    dist_y = wall.top - self.ball_sprite.bottom
+                    if dist_x > dist_y:
+                        #print("Go Top_Right")
+                        #print(dist_x, dist_y)
+                        self.world.ball.bounce_to_top()
+                        self.world.ball.prev_pos()
+                    elif dist_x == dist_y:
+                        #print("Go Reverse")
+                        self.world.ball.bounce_reverse()
+                        self.world.ball.prev_pos()
+                    else:
+                        #print("Go Left_Bottom")
+                        self.world.ball.bounce_to_left()
+                        self.world.ball.prev_pos()
+                else:
+                    #print("Go Left_?")
+                    self.world.ball.bounce_to_left()
+                    self.world.ball.prev_pos()
+            else:
+                #print("Go Top/Bottom")
+                if self.ball_sprite.center_y < wall.change_y:
+                    self.world.ball.bounce_to_bottom()
+                else:
+                    self.world.ball.bounce_to_top()
+                self.world.ball.prev_pos()
+
         ###
         if TWO_PLAYER:
             if arcade.check_for_collision(self.ball_sprite, self.player2_sprite) :
